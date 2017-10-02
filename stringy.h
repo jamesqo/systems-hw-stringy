5fc4cae89c56dbc926436116e6b3df4a3aa4eca1
James Ko <jamesqko@gmail.com>
2017-10-02 10:43:14 -0400

    Initial commit of C

---
+++ stringy.h
@@ -0,0 +1,11 @@
+char* our_strncpy(char* dest, char* source);
+
+char * our_strcat( char *dest, char *source );
+
+int our_strlen( char * );
+
+int our_strcmp( char *s1, char *s2 );
+
+char * our_strchr( char *s, char c );
+
+char * our_strstr( char *s1, char * s2 )
